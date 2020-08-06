e67fd23af7d38a077eb613004feb0c5b9a022275
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2020-08-05 20:31:24 -0700

    Basic button working

--- lewdo/system/cube.js
+++ lewdo/system/cube.js
@@ -2,14 +2,37 @@
 // TODO: fix this for all HTML displays:
 
 var _lewdo_square_string = "┌-┐\n┃ ┃\n└-┘";
-var _lewdo_square_corners_string = _lewdo_square_string;
+var _lewdo_square_corners_string = "┌ ┐\n   \n└ ┘";
+//var _lewdo_square_string = "/-\\\n┃ ┃\n\\-/";
+//var _lewdo_square_corners_string = "/ \\\n   \n\\ /";
 var _lewdo_cube_string = _lewdo_square_string + "\v" + _lewdo_square_corners_string + "\v" + _lewdo_square_string;
 
-var lewdo_cube = {
-
+var lewdo_square = {
     square_string : _lewdo_square_string,
+    square_string3 : string3( _lewdo_square_string ),
     square_corners_string : _lewdo_square_corners_string,
+    square_corners_string3 : string3( _lewdo_square_corners_string ),
 
+    aroundString3 : function(center) {
+        var ans = string3();
+        var t = string3_utils.xyz();
+        ans.resize(center.width+2, center.height+2, Math.max(1,center.depth) );
+        var ansSize = ans.sizeXYZ();
+        string3_utils.visitEachXYOnZ(ans,0,(letter,v) => {
+            if (true || ans.isEdgeXYZ(v)) {
+                t.copy(v);
+                t.select2( ansSize, lewdo_cube._clampIndex );
+                t.z = 0;
+                var val = lewdo_square.square_string3.getByXYZ(t);
+                ans.setByXYZ(val, v);
+            }
+        });
+        ans.drawString3XYZ(center,string3_utils.xyz(1,1,0));
+        return ans;
+    },
+};
+
+var lewdo_cube = {
     cube_string : _lewdo_cube_string,
     cube_string3 : string3( _lewdo_cube_string ),
 
---
+++ lewdo/system/shapes/button.js
@@ -0,0 +1,31 @@
+
+var lewdo_button_app = function(app,content,callback) {
+    if (!content) {
+        content = string3("button");
+    }
+    if (!callback) {
+        callback = (() => {console.log("button clicked.")});
+    }
+    var isClicking = false;
+    app.app_in.subscribe((input) => {
+        if (input.width > 0) {
+            var str = input.toString();
+            if (str == "Enter" || str == "►") {
+                isClicking = true;
+                content = string3("clicked");
+                callback();
+            }
+        } else {
+            if (isClicking) {
+                isClicking = false;
+                content = string3("button again");
+            }
+        }
+
+        app.app_out.copy( lewdo_square.aroundString3(content) );
+        app.app_out.frameStep();
+    });
+}
+
+
+lewdo_app_prototype.all_apps.system["button"] = lewdo_button_app;
