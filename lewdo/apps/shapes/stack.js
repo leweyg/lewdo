50b14c7bed911ad45f295d1bc35ae2cfdaa1cbae
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2020-08-27 11:53:13 -0700

    Ensure that stack offset isn't exposed to app itself

--- lewdo/apps/shapes/stack.js
+++ lewdo/apps/shapes/stack.js
@@ -29,7 +29,12 @@ var lewdo_stack = {
         myApp : lewdo_app(),
         axis : "y",
         pushApp : function(app) {
-            this.stackedApps.push(app);
+            this.stackedApps.push({
+                app:app,
+                index:(this.stackedApps.length),
+                align:"center",
+                offset : string3_utils.xyz(),
+            });
 
             var _this = this;
             app.app_out.subscribe(() => _this.redraw());
@@ -42,7 +47,7 @@ var lewdo_stack = {
             var maxSizes = string3_utils.xyz(1,1,1);
             var sumSizes = string3_utils.xyz(1,1,1);
             for (var si in this.stackedApps) {
-                var size = this.stackedApps[si].app_out.sizeXYZ();
+                var size = this.stackedApps[si].app.app_out.sizeXYZ();
                 maxSizes.select2(size, (was,cur) => Math.max(was,cur));
                 sumSizes.select2(size, (was,cur) => (was + cur));
             }
@@ -54,8 +59,9 @@ var lewdo_stack = {
             to.resize(finalSize.x,finalSize.y,finalSize.z, ' ');
             var drawOffset = string3_utils.xyz(0,0,0);
             for (var si in this.stackedApps) {
-                var src = this.stackedApps[si].app_out;
-                src.offset.copy(drawOffset);
+                var stackItem = this.stackedApps[si];
+                var src = stackItem.app.app_out;
+                stackItem.offset.copy(drawOffset);
                 if (isRender) {
                     to.drawString3XYZ(src, drawOffset);
                 }
@@ -74,10 +80,11 @@ var lewdo_stack = {
             var originalOffset = input.offset;
             var t = string3_utils.xyz();
             for (var si in this.stackedApps) {
-                var sapp = this.stackedApps[si];
-                var aout = sapp.app_out;
+                var stackItem = this.stackedApps[si];
+                var sapp = stackItem.app;
+                var aout = stackItem.app.app_out;
                 t.copy(input.offset);
-                t.minus(aout.offset);
+                t.minus(stackItem.offset);
                 if (aout.isValidXYZ(t)) {
                     input.offset = t;
                     sapp.app_in.copy(input);
