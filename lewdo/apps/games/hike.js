20ca6961128e9c5d01ef51d30b58911355d3a7c8
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2020-09-01 21:41:12 -0700

    In mid-tuning

--- lewdo/apps/games/hike.js
+++ lewdo/apps/games/hike.js
@@ -11,11 +11,152 @@ var lewdo_hike = {
     },
     lewdo_hike_prototype : {
         app : lewdo_app(),
+        displaySize : lewdo.xyz(16,9,5),
+        gradient : ".:-+*=%@#",
+        fracalScale : 1,
+        maxSteps : 10,
 
         setup : function(_app) {
             this.app = _app;
-            this.app.app_out.copy(string3("\nhike\vlewdo"));
-        }
+
+            this.redraw();
+            //this.app.app_out.copy(string3("\nhike\vlewdo"));
+            this.fracalScale++;
+
+            this.app.app_in.subscribe((input) => {
+                var was = this.fracalScale;
+                if (this.fracalScale != was) {
+                    this.redraw();
+                }
+            });
+        },
+        redraw : function() {
+            var display = this.app.app_out;
+            display.resizeXYZ(this.displaySize);
+            display.modifyEachXYZ((was,pos)=> {
+                var fracValue = this.mandelbrot(
+                    pos.x*this.fracalScale,
+                    pos.y*this.fracalScale,
+                    this.maxSteps) / this.maxSteps;
+                if ((fracValue == 0))
+                    return " ";
+                var t = Math.floor(fracValue * this.gradient.length) % this.gradient.length;
+                if ((this.displaySize.z - pos.z) > t) return " ";
+                return this.gradient[t];
+            });
+            display.frameStep();
+        },
+        mandelbrot : function (cx, cy, maxIter) {
+            var x = 0.0;
+            var y = 0.0;
+            var xx = 0;
+            var yy = 0;
+            var xy = 0;
+           
+            var i = maxIter;
+            while (i-- && xx + yy <= 4) {
+              xy = x * y;
+              xx = x * x;
+              yy = y * y;
+              x = xx - yy + cx;
+              y = xy + xy + cy;
+            }
+            return maxIter - i;
+        },
+        debugUI_HTML : function() {
+            var canvas = document.createElement('canvas');
+            canvas.width = 160;
+            canvas.height = 90;
+             
+
+            var controlsBlock = document.createElement("p");
+            controlsBlock.appendChild(canvas);
+            controlsBlock.appendChild(document.createElement("br"));
+
+            document.body.appendChild(controlsBlock);
+            this.mandelbrotIntoCanvas(canvas, -2, 1, -1, 1, 1000);
+
+            var options = {
+                xmin : 0.1,// -2,
+                xmax : 0.15,// 1,
+                ymin : 0.62, //-1,
+                ymax : 0.65,// 1,
+                iterations : 150,
+            };
+
+            var _this = this;
+            var refractal = function() {
+                _this.mandelbrotIntoCanvas(canvas,
+                    options.xmin, options.xmax,
+                    options.ymin, options.ymax,
+                    options.iterations);
+            };
+            refractal();
+
+            var makeOption = ((optionName) => {
+                var label = document.createElement("span");
+                label.innerHTML = optionName;
+                controlsBlock.appendChild(label);
+
+                var val = document.createElement("input");
+                val.type = "text";
+                val.value = options[optionName];
+                val.onchange = (() => {
+                    options[optionName] = 1.0 * val.value;
+                    refractal();
+                });
+                controlsBlock.appendChild(val);
+
+                controlsBlock.appendChild(document.createElement("br"));
+            });
+             
+            for (var optionName in options) {
+                makeOption(optionName);
+            }
+        },
+        mandelbrotIntoCanvas : function(canvas, xmin, xmax, ymin, ymax, iterations) {
+            var width = canvas.width;
+            var height = canvas.height;
+           
+            var ctx = canvas.getContext('2d');
+            var img = ctx.getImageData(0, 0, width, height);
+            var pix = img.data;
+           
+            for (var ix = 0; ix < width; ++ix) {
+              for (var iy = 0; iy < height; ++iy) {
+                var x = xmin + (xmax - xmin) * ix / (width - 1);
+                var y = ymin + (ymax - ymin) * iy / (height - 1);
+                var i = this.mandelbrot(x, y, iterations);
+                var ppos = 4 * (width * iy + ix);
+           
+                if (i > iterations) {
+                  pix[ppos] = 0;
+                  pix[ppos + 1] = 0;
+                  pix[ppos + 2] = 0;
+                } else {
+                  var c = 3 * Math.log(i) / Math.log(iterations - 1.0);
+           
+                  if (c < 1) {
+                    pix[ppos] = 255 * c;
+                    pix[ppos + 1] = 0;
+                    pix[ppos + 2] = 0;
+                  }
+                  else if ( c < 2 ) {
+                    pix[ppos] = 255;
+                    pix[ppos + 1] = 255 * (c - 1);
+                    pix[ppos + 2] = 0;
+                  } else {
+                    pix[ppos] = 255;
+                    pix[ppos + 1] = 255;
+                    pix[ppos + 2] = 255 * (c - 2);
+                  }
+                }
+                pix[ppos + 3] = 255;
+              }
+            }
+           
+            ctx.putImageData(img, 0, 0);
+          }
     }
 };
 
