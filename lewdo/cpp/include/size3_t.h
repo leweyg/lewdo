3b0c13cbd8acb5b26639980dc8f02a507cf97c87
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2020-09-30 20:16:06 -0700

    Order refactor

--- lewdo/cpp/include/size3_t.h
+++ lewdo/cpp/include/size3_t.h
@@ -10,7 +10,7 @@
 
 namespace lewdo {
 
-    template <typename T,size_t N> class tensorN_T {
+    template <size_t N,typename T> class tensorN_T {
     public:
         T v[N];
         
@@ -21,7 +21,7 @@ namespace lewdo {
 
     #define EXPAND3_i(foo) {const size_t i=0;foo;}{const size_t i=1;foo;}{const size_t i=2;foo;}
 
-    class size3_t : public tensorN_T<size_t,3> {
+    class size3_t : public tensorN_T<3,size_t> {
     public:
         static const size_t N = 3;
         static const size3_t zero;
